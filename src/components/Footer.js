142d0788a47dbf684de25f08c7d05fcd64cc53e7
Dakotah Walker <dwalker@contactaac.com>
2018-08-16 11:36:54 -0700

    Adds top padding to footer at lg (992) breakpoint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,18 @@
 import React from 'react';
-import LogoSecondary from './logo/LogoSecondary';
-import aacHorizontal from '../assets/logos/aac-horiz-white.svg';
+// data
 import { facilityInfo } from '../lib/project-config.js';
 import { phoneNumber } from '../lib/globals.js';
-
-// imports icons for SocialList
+// icons/logos
 import FacebookIcon from '../assets/icons/facebook-white.svg';
 import InstagramIcon from '../assets/icons/instagram-white.svg';
 import LinkedInIcon from '../assets/icons/linkedin-white.svg';
 import TwitterIcon from '../assets/icons/twitter-white.svg';
-// imports logos for Accredidations List
+import AacHorizontalLogo from '../assets/logos/aac-horiz-white.svg';
 import BBBLogo from '../assets/logos/bbb.png';
 import CarfLogo from '../assets/logos/carf-white.png';
 import JointCommissionLogo from '../assets/logos/joint-commission-white.png';
 
-const DisclaimersList = () => (
-  <ul className="subtext text-grey-100 list-reset pt-20 pl-10">
-    <li className="leading-normal pb-5 lg:pb-10">
-      *Source:
-      <a
-        href="https://www.niaaa.nih.gov/alcohol-health/overview-alcohol-consumption/alcohol-use-disorders"
-        target="blank"
-      >
-        www.niaaa.nih.gov/alcohol-health/overview-alcohol-consumption/alcohol-use-disorders
-      </a>
-    </li>
-    <li className="leading-normal">
-      **Terms and conditions apply. To learn more about how you can qualify for
-      our guarantee, visit{' '}
-      <a href="https://americanaddictioncenters.org/disclaimer/" target="blank">
-        americanaddictioncenters.org/disclaimer
-      </a>{' '}
-      for details.
-    </li>
-  </ul>
-);
+const logoWidth = { maxWidth: '290px' };
 
 export default function Footer() {
   const date = new Date();
@@ -42,8 +20,8 @@ export default function Footer() {
   return (
     <footer className="bg-navy flex flex-col">
       <div className="lg:flex">
-        <div className="lg:w-1/3 footer-section my-8 text-center">
-          <img src={aacHorizontal} style={{ width: '290px', height: 'auto' }} />
+        <div className="footer-section my-8 text-center lg:mt-16 lg:w-1/3">
+          <img src={AacHorizontalLogo} style={logoWidth} />
           <p className="p text-white leading-normal my-8">
             American Addiction Centers <br />
             {phoneNumber} <br />
@@ -68,10 +46,10 @@ export default function Footer() {
             </a>
           </div>
         </div>
-        <div className="lg:mt-8 lg:w-1/3 footer-section pt-4">
+        <div className="pt-4 lg:mt-12 lg:w-1/3">
           <h3 className="p text-aqua mb-8 px-4">Privacy</h3>
           <ul className="list-reset mt-8 mb-4 px-8 pb-4">
-            <li className="mb-2 text-aqua">
+            <li className="mb-2 text-aqua" style={logoWidth}>
               <a
                 className="subtext"
                 href={facilityInfo.onlinePolicyLink}
@@ -80,7 +58,7 @@ export default function Footer() {
                 Notice of Privacy Practices
               </a>
             </li>
-            <li className="mb-2 text-aqua">
+            <li className="mb-2 text-aqua" style={logoWidth}>
               <a
                 className="subtext"
                 href={facilityInfo.privatePolicyLink}
@@ -89,7 +67,7 @@ export default function Footer() {
                 Online Privacy Policy
               </a>
             </li>
-            <li className="mb-2 text-aqua">
+            <li className="mb-2 text-aqua" style={logoWidth}>
               <a
                 className="subtext"
                 href={facilityInfo.termsLink}
@@ -100,7 +78,7 @@ export default function Footer() {
             </li>
           </ul>
           <div
-            className="flex justify-between m-auto lg:ml-8 pb-16"
+            className="flex justify-between ml-8 pb-16"
             style={{ width: '220px' }}
           >
             <img src={BBBLogo} alt="Better Business Bureau " />
@@ -108,11 +86,11 @@ export default function Footer() {
             <img src={JointCommissionLogo} alt="Joint Commission " />
           </div>
         </div>
-        <div className="lg:mt-8 lg:w-1/3 footer-section pt-4">
+        <div className="pt-4 lg:mt-12 lg:w-1/3">
           <h3 className="p text-aqua mb-8 px-4">Resources</h3>
           <ul className="list-reset mt-8 mb-4 px-8 pb-4">
-            <li className="subtext text-aqua mb-2">
-              *Source:{' '}
+            <li className="subtext text-aqua mb-2" style={logoWidth}>
+              * Source:{' '}
               <a
                 href="https://www.niaaa.nih.gov/alcohol-health/overview-alcohol-consumption/alcohol-use-disorders"
                 target="blank"
@@ -120,8 +98,8 @@ export default function Footer() {
                 www.niaaa.nih.gov
               </a>
             </li>
-            <li className="subtext text-aqua mb-2">
-              **Terms and conditions apply. To learn more about how you can
+            <li className="subtext text-aqua mb-2" style={logoWidth}>
+              ** Terms and conditions apply. To learn more about how you can
               qualify for our guarantee, read our{' '}
               <a
                 href="https://americanaddictioncenters.org/disclaimer/"

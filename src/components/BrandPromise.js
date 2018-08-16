3d98406240a421d952af64a6fe345c6b4ab4c79c
Dakotah Walker <dwalker@contactaac.com>
2018-08-16 14:51:34 -0700

    Refactors BrandPromise Content Block
    - BrandPromise does not carry internal state, so we refactor it into a
    named function expression to avoid potential issues when debugging
    (e.g.) arrow functions are anonymous and thus harded to debug

--- src/components/BrandPromise.js
+++ src/components/BrandPromise.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PromiseSeal from '../assets/logos/90-day-promise.png';
 
-const BrandPromise = () => (
-  <section className="bg-grey-100 py-40">
-    <div className="container m-auto px-10 lg:px-40">
-      <div className="w-48 m-auto mb-20">
-        <img src={PromiseSeal} alt="90 day promise" />
-      </div>
-      <p className="p text-center text-grey-500 leading-normal lg:m-auto lg:max-w-lg">
-        If you successfully complete our 90-day program, we guarantee you’ll
-        stay clean and sober, or you can return for a complimentary 30 days of
-        treatment. **
-      </p>
-    </div>
-  </section>
-);
+/**
+ * Potential global class rules:  element - classes
+ *
+ * section - section
+ * div - {style: logoWidth}
+ *
+ */
 
-export default BrandPromise;
+const logoWidth = { maxWidth: '290px' };
+
+export default function BrandPromise() {
+  return (
+    <section className="section bg-grey-100">
+      <div className="content-block text-center" style={logoWidth}>
+        <img src={PromiseSeal} alt="aac 90 day promise" />
+        <p className="p py-8">
+          If you successfully complete our 90-day program, we guarantee you’ll
+          stay clean and sober, or you can return for a complimentary 30 days of
+          treatment. **
+        </p>
+      </div>
+    </section>
+  );
+}

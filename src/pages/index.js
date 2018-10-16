34821cd1085dfb32c9a441c14b610822f1d1e361
Dakotah Walker <dwalker@contactaac.com>
2018-10-16 15:50:53 -0700

    Refactors Insurance Block
    - adds stateless image function to return styled logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { project } from "../lib/project-config"
 import Hero from '../components/Hero/Hero';
 import FacilityFeatures from '../components/FacilityFeatures';
-
+import Insurance from "../components/Insurance"
 import { heroDefault } from "../lib/content-config"
 
 export default function IndexPage() {
   return (
     <div>
+
       <FacilityFeatures theme={project} />
+      <Insurance />
     </div>
   );
 }
